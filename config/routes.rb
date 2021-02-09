66b94164184ab97a855c9ab8efe996a24f23776b
mapi8808 <mapi.008.k@gmail.com>
2021-02-09 01:54:49 +0000

    内容修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
+  #ユーザールーティング自動設定
   resources :users, only: [:show, :edit, :quit, :update, :goodbye]
   root to: 'homes#top'
-  # 商品ルーティング自動設定
+  #商品ルーティング自動設定
   resources :products
-  
+  #ショップルーティング自動設定
+  resources :shops, only: [:show, :edit, :new, :create, :update, :destroy]
  
 end
