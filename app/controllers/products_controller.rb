9e4266cf08f08169094c7330d874fa6afb53b2e3
mapi8808 <mapi.008.k@gmail.com>
2021-02-09 08:08:05 +0000

    [add]productsコントローラー修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,12 +17,24 @@ class ProductsController < ApplicationController
   end
   
   def create
+    @product = Product.new(product_params)
+    product.save
+    redirect_to product_path
   end
   
   def update
+    @product = Product.find(params[:id])
+    product.update(product_params)
+    redirect_to product_path
   end
   
   def destroy
+  end
+  
+  private
+  
+  def product_params
+    params.require(:product).permit(:product_name, :image, :body, :amounts)
   end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  devise_for :users
   get 'about' => 'homes#about'
   get 'contact' => 'homes#contact'
-  
+  root to: 'homes#top'
+  devise_for :users
   #ユーザールーティング自動設定
   resources :users, only: [:show, :edit, :quit, :update, :goodbye]
   #商品ルーティング自動設定
   resources :products
   #ショップルーティング自動設定
   resources :shops, only: [:show, :edit, :new, :create, :update, :destroy]
- 
+
 end
