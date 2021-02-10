24e0d9135e16d813b4e9ecb2cd4aa2073477a7c3
mapi8808 <mapi.008.k@gmail.com>
2021-02-10 14:54:57 +0000

    [add]adminを作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,30 @@
-class UsersController < ApplicationController
-  
+class Users::UsersController < ApplicationController
+
   def show
     @user = User.find(params[:id])
   end
-  
+
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def quit
   end
-  
+
   def update
     @user = User.find(params[:id])
     user.update(user_params)
     redirect_to user_path
   end
-  
+
   def goodbye
   end
-  
+
   private
-  
+
   def user_params
     params.require(:user).permit(:email, :name,)
   end
-  
-  
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,27 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admins, skip: :all
+  devise_scope :admin do
+    get 'admins/sign_in' => 'admins/sessions#new', as: 'new_admin_session'
+    post 'admins/sign_in' => 'admins/sessions#create', as: 'admin_session'
+    delete 'admins/sign_out' => 'admins/sessions#destroy', as: 'destroy_admin_session'    
+  end
+  
+  devise_for :users, skip: :all
+  devise_scope :user do
+    get 'users/sign_in' => 'users/sessions#new', as: 'new_user_session'
+    post 'users/sign_in' => 'users/sessions#create', as: 'user_session'
+    delete 'users/sign_out' => 'users/sessions#destroy', as: 'destroy_user_session'
+    get 'users/sign_up' => 'users/registrations#new', as: 'new_user_registration'
+    post 'users' => 'users/registrations#create', as: 'user_registration'
+    get 'users/password/new' => 'users/passwords#new', as: 'new_user_password'
+  end
+  
    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :users do
+    
   get 'about' => 'homes#about'
   get 'contact' => 'homes#contact'
   root to: 'homes#top'
-  devise_for :users
   #ユーザールーティング自動設定
   resources :users, only: [:show, :edit, :quit, :update, :goodbye]
   #商品ルーティング自動設定
@@ -13,5 +30,5 @@ Rails.application.routes.draw do
   end
   #ショップルーティング自動設定
   resources :shops, only: [:show, :edit, :new, :create, :update, :destroy]
-
+  end
 end
