28bb668407eca331605201c947cd249722f6d56c
mapi8808 <mapi.008.k@gmail.com>
2021-02-10 05:24:06 +0000

    [add]bootstrapを導入

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_09_143800) do
+ActiveRecord::Schema.define(version: 2021_02_09_145720) do
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "product_name"
